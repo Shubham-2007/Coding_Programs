1fa4f0555bd8ce96a164adac31bc80b256281134
Priyal Kathrani <87109435+priyalkathrani@users.noreply.github.com>
2021-10-02 18:19:06 +0530

    Add files via upload

---
+++ priyalkathrani/most_viewed_screen/most_viewed_sreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(mvv_screen()); 
+
+class mvv_screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(backgroundColor: Colors.orange,),
+
+    );
+  }
+}
